edb5bd3cfa356e87d4dd772551573bb58a434575
bawyap <bawya.p1@thoughtworks.com>
2020-09-26 10:18:07 +0530

    moved  test name

--- src/test/java/com/testing/DivisionTest.java
+++ src/test/java/com/testing/DivisionTest.java
@@ -9,6 +9,13 @@ public class DivisionTest {
 
 
     @Test
+    public void testZero() {
+        int[] values = Division.getValues(1, 0);
+
+        assertNull(values);
+    }
+
+    @Test
     public void testGetValues_V1() {
         int[] values = Division.getValues(1, 1);
 
@@ -16,13 +23,6 @@ public class DivisionTest {
         int expectedReminder = 0;
         assertEquals(expectedQuotient, values[0]);
         assertEquals(expectedReminder, values[1]);
-    }
-
-    @Test
-    public void testZero() {
-        int[] values = Division.getValues(1, 0);
-
-        assertNull(values);
     }
 
     @Test
